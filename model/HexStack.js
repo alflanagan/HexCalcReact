b646e075daa9bd16d875bfeb81651dfc4aaab840
Adrian (Lloyd) Flanagan <aflanagan@bhmginc.com>
2018-10-18 12:36:21 -0400

    I can spell 'hexadecimal', I swear

--- model/HexStack.js
+++ model/HexStack.js
@@ -1,14 +1,19 @@
 /**
- * A stack of numbers, with special operations for treating the values as hexidecimal
+ * A stack of numbers, with special operations for treating the values as hexadecimal
  * numbers (possibly with a fixed word size).
  */
 
 export default class HexStack {
+  /**
+   * Create a new stack of numbers.
+   * @param {[object]} iterable An object whose iterator enumerates a series of integers, or
+   *   strings which are valid hexadecimal numbers.
+   */
   constructor (iterable) {
     this.values = []
     if (typeof iterable !== 'undefined' && iterable !== null) {
       for (let value of iterable) {
-        this.values.push(value)
+        this.values.push(typeof value === 'string' ? Number.parseInt(value, 16) : value)
       }
     }
   }
