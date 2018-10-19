4d74c2254d8040b4ad2ff3d96e53086627e15d94
Adrian (Lloyd) Flanagan <aflanagan@bhmginc.com>
2018-10-19 16:43:13 -0400

    Add stack for fixed-word-size numbers, signed and unsigned

---
+++ src/model/SizedHexStack.js
@@ -0,0 +1,106 @@
+/**
+ * HexStack
+ * A stack of bigints, with special operations for treating the values as hexadecimal
+ * numbers (possibly with a fixed word size).
+ *
+ * Note we use BigInt, not Number, bedcause we want to support at least 64 bits.
+ */
+
+// Here we're explicitly using a Delegation pattern rather than Inheritance. It reflects how
+// Javascript actually implements things, unlike the "standard" use of `new` and `prototype`,
+// even with the new `class` syntax
+
+// instead of `new HexStack()` say
+// `Object.create(HexStack).init()`
+
+
+export default SizedHexStack = {
+
+/**
+ * Set up this object.
+ * @param  {Iterable}  iterable      An iterable producing values to be pushed onto this stack.
+ *     {@see push()} for a description of legal values.
+ * @param  {Boolean} signed   Whether to treat the values as signed or unsigned. Only
+ *     meaningful when `bits` is non-zero, in which case negative values
+ * @param  {Number}  bits      Maximum number of bits in input and output values.
+ * @return {HexStack}              The object, set up according to params.
+ */
+  init (iterable, signed, bits) {
+    this.values = []
+    this.signed = signed
+    this.bits = bits
+
+    if (typeof iterable !== 'undefined' && iterable !== null) {
+      for (let value of iterable) {
+        this.push(value)
+      }
+    }
+    return this
+  },
+
+  toString () {
+    return this.values.map(val => BigInt.toString(val, 16)).join('\n')
+  },
+
+  pop () {
+    return this.values.length === 0 ? null : this.values.pop()
+  },
+
+  push (value) {
+    const _ = (num) => this.signed ? BigInt.asIntN(this.bits, num) : BigInt.asUintN(this.bits, num)
+    switch (typeof value) {
+      case 'bigint':
+        this.values.push(_(value))
+        break
+      case 'number': // throws RangeError if not an int (fractional part == 0)
+        this.values.push(_(BigInt(value)))
+        break
+      case 'string': // throws SyntaxError if not valid integer
+        // assumes string in hexadecimal format, even if not explicit
+        if (!(value.startsWith('0x') || value.startsWith('0X'))) {
+          value = `0x${value}`
+        }
+        this.values.push(_(BigInt(value)))
+        break
+      default:
+        throw new TypeError(`HexStack doesn't know how to accept ${value}.`)
+    }
+  },
+
+  op (operator) {
+    let a, b
+    switch (operator) {
+      case '+':
+        this.push(this.pop() + this.pop())
+        break
+      case '-':
+        a = this.pop()
+        b = this.pop()
+        this.push(b - a)
+        break
+      case '*':
+        this.push(this.pop() * this.pop())
+        break
+      case '/':
+        a = this.pop()
+        b = this.pop()
+        this.push(b / a)
+        break
+      case '%':
+        a = this.pop()
+        b = this.pop()
+        this.push(b % a)
+        break
+      default:
+        throw new RangeError(`Operator not recognized: '${operator}'`)
+    }
+  },
+
+  clear () {
+    this.values.clear()
+  },
+
+  size () {
+    return this.values.length
+  }
+}
---
+++ src/model/SizedHexStack.spec.js
@@ -0,0 +1,81 @@
+/**
+ * Unit testing for SizedHexStack.js
+ */
+import SizedHexStack from './SizedHexStack'
+
+/**
+ * Retrieve values from the stack into an array to allow easy comparison to expected values.
+ * @param  {SizedHexStack} stack The stack being tested.
+ * @return {Array<String>}       An array of the integer values, from bottom of stack to top, as
+ *   strings to provide better error messages.
+ */
+function getValues (stack) {
+  const arr = []
+  let value = stack.pop()
+  while (value !== null) {
+    arr.push(value)
+    value = stack.pop()
+  }
+  // restore the stack as it was
+  for (let i = arr.length - 1; i >=0; i--) {
+    stack.push(arr[i])
+  }
+  return arr.map((v) => v.toString(16))
+}
+
+test('I can create an empty stack.', () => {
+  expect(getValues(Object.create(SizedHexStack).init([], false, 32))).toEqual([])
+})
+
+test('The getValues() function correctly restores the stack', () => {
+  var stack = Object.create(SizedHexStack).init([1, 2, 3, 4], false, 32)
+  expect(getValues(stack)).toEqual(['4', '3', '2', '1'])
+  expect(stack.size()).toBe(4)
+  expect(getValues(stack)).toEqual(['4', '3', '2', '1'])
+})
+
+test('I can create a stack from an array of integers.', () => {
+  var stack = Object.create(SizedHexStack).init([1, 2, 3, 4], false, 8)
+  expect(getValues(stack)).toEqual(['4', '3', '2', '1'])
+})
+
+test('Integers in a stack are truncated to the given number of bits.', () => {
+  var stack = Object.create(SizedHexStack).init([257, 514, 259, 0xF04], false, 8)
+  expect(getValues(stack)).toEqual(['4', '3', '2', '1'])
+})
+
+test('I can create a stack from an array of strings.', () => {
+  const newStack = Object.create(SizedHexStack).init(['FACE', 'BEEF', 'BAD'], false, 32)
+  expect(getValues(newStack)).toEqual(['bad', 'beef', 'face'])
+  // either with or without '0x' prefix works
+  const newStack2 = Object.create(SizedHexStack).init(['0xFACE', '0xBEEF', '0xBAD'], false, 16)
+  expect(getValues(newStack2)).toEqual(['bad', 'beef', 'face'])
+})
+
+test('Creating a stack fails if iterator returns neither a number or string.', () => {
+  const newStack = Object.create(SizedHexStack)
+  expect(() => { newStack.init([{ value: '1234' }], true, 23) }).toThrow(TypeError)
+})
+
+test('Creating a stack fails if given a non-hexadecimal string.', () => {
+  const newStack = Object.create(SizedHexStack)
+  expect(() => { newStack.init(['FACE', 'NOTHEX'], false, 8) }).toThrow(SyntaxError)
+})
+
+test('I can push and pop a large value.', () => {
+  const newStack = Object.create(SizedHexStack).init([], false, 128)
+  const testVal = BigInt('0xAB43890FED123421187843')
+  newStack.push(testVal)
+  expect(newStack.pop()).toEqual(testVal)
+  expect(newStack.size()).toEqual(0)
+})
+
+test('A signed stack can return negative numbers', () => {
+  const stack = Object.create(SizedHexStack).init([-5], true, 16)
+  expect(stack.pop().toString(16)).toEqual('-5')
+})
+
+test('An unsigned stack returns twos-comlement of negative numbers', () => {
+  const stack = Object.create(SizedHexStack).init([-5], false, 16)
+  expect(stack.pop().toString(16)).toEqual('fffb')
+})
