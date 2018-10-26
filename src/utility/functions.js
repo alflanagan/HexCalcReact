009818a40bcaf1a5fb7cc9063de46b7e7a1129b1
Adrian (Lloyd) Flanagan <aflanagan@bhmginc.com>
2018-10-26 12:54:41 -0400

    :ambulance: Add big-integer, repro. BigInt asIntN asUintN
    Finally got to point where I discovered react-native doesn't support BigInt on android.

--- src/utility/functions.js
+++ src/utility/functions.js
@@ -2,7 +2,9 @@
  * utility/functions.js
  * A module of useful (?) general-purpose functions
  */
-import { keys } from 'ramda'
+import { keys, lt } from 'ramda'
+// big-integer doesn't support standard modules (yet)
+const bigInt = require('big-integer')
 
 export function safeToString (something) {
   if (typeof something === 'undefined') { return 'undefined' }
@@ -11,6 +13,23 @@ export function safeToString (something) {
   return something.toString()
 }
 
+/**
+ * Returns array of given length, every item initialized to initialValue (there's got to be
+ * some sort of standard function for this!)
+ *
+ * TODO: accept a function for initialValue, which is (index) => any.
+ *
+ * @param  {integer} length       The length of the returned array.
+ * @param  {any} initialValue Value assigned to each index of the array.
+ * @return {Array}              Initialized array.
+ */
+export function arrayOf (length, initialValue) {
+  const newArray = Array(length)
+  for (let i = 0; i < length; i++) {
+    newArray[i] = initialValue
+  }
+  return newArray
+}
 /**
  * Converts object to more-useful string form
  * @param  {object} something Any object
@@ -24,4 +43,36 @@ export function objToString (something) {
   }
   // fallback to builtin
   return something.toString()
+}
+
+export function bigIntAsIntN (numbits, value) {
+  if (typeof numbits !== 'number' || !bigInt.isInstance(value)) {
+    throw new TypeError('bigIntAsIntN requires a number and a bigInt')
+  }
+  if (lt(0, value)) {
+    return value.mod(bigInt(2).pow(numbits))
+  } else {
+    // get positive number from truncating to 1 fewer bits than numbits
+    const positive = value.abs().mod(bigInt(2).pow(numbits - 1))
+    // get the two's complement of that value
+    // convert number to array of 1s and 0s
+    const bits = positive.toArray(2).value
+    // pad number with 0s to numbits
+    const padZeros = numbits - bits.length
+    const padding = arrayOf(padZeros, 0)
+    const bitArray = padding.concat(bits)
+    if (bitArray.length !== numbits) {
+      throw new Error('Created a bitArray of wrong length -- this is a bug!')
+    }
+    const onesComp = Array(bitArray.length)
+    for (let i = 0; i < numbits; i++) {
+      onesComp[i] = bitArray[i] === 0 ? 1 : 0
+    }
+    let bigOnesComp = bigInt.fromArray(onesComp, 2)
+    return bigOnesComp.plus(1)
+  }
+}
+
+export function bigIntAsUintN (numbits, value) {
+  return value.abs().mod(bigInt(2).pow(numbits))
 }
--- src/utility/functions.spec.js
+++ src/utility/functions.spec.js
@@ -1,7 +1,7 @@
+import { safeToString, objToString, bigIntAsUintN, bigIntAsIntN } from './functions'
+const bigInt = require('big-integer')
 
-import { safeToString, objToString } from './functions'
-
-describe('tests of safeToString function', () => {
+describe('safeToString converts a value to a string, even if it is null or undefined', () => {
   test('safeToString of undefined is "undefined"', () => {
     expect(safeToString(undefined)).toEqual('undefined')
   })
@@ -14,12 +14,12 @@ describe('tests of safeToString function', () => {
     expect(safeToString(1)).toEqual('1')
   })
 
-  test('safeToSting of "fred" is "fred"', () => {
+  test('safeToString of "fred" is "fred"', () => {
     expect(safeToString('fred')).toEqual('"fred"')
   })
 })
 
-describe('tests of objToString() function', () => {
+describe('objToString() converts object to useful string representation', () => {
   test('objToString of undefined is "undefined"', () => {
     expect(objToString(undefined)).toEqual('undefined')
   })
@@ -46,5 +46,65 @@ describe('tests of objToString() function', () => {
 
   test('objToString renders multiple properties correctly', () => {
     expect(objToString({ a: 12, b: 'fred', c: null })).toEqual('{a: 12, b: "fred", c: null}')
+  })
+})
+
+describe('bigIntAsUintN() truncates an unsigned bigInt to a set number of bits', () => {
+  test('correctly truncates to 16 bits', () => {
+    const fred = bigInt(0x12345AB)
+    const wilma = bigIntAsUintN(16, fred)
+    expect(bigInt.isInstance(wilma)).toBe(true)
+    expect(wilma.toJSNumber()).toEqual(0x45AB)
+  })
+
+  test('does not change number already smaller than spedified', () => {
+    const fred = bigInt(0x123B)
+    const wilma = bigIntAsUintN(16, fred)
+    expect(bigInt.isInstance(wilma)).toBe(true)
+    expect(wilma.toJSNumber()).toEqual(0x123B)
+  })
+})
+
+describe('bigIntAsIntIn() truncates a signed bigInt to a set number of bits', () => {
+  test('correctly truncates positive int to 32 bits', () => {
+    const fred = bigInt('123456789ABCDEF0', 16)
+    const wilma = bigIntAsIntN(32, fred)
+    expect(bigInt.isInstance(wilma))
+    expect(wilma.toJSNumber()).toEqual(0x9ABCDEF0)
+  })
+
+  test('correctly truncates negative int to 32 bits', () => {
+    const fred = bigInt('-123456789ABCDEF0', 16)
+    const wilma = bigIntAsIntN(32, fred)
+    expect(bigInt.isInstance(wilma))
+    expect(wilma.toJSNumber()).toEqual(0xE5432110)
+  })
+
+  test('correctly truncates negative to 16 bits', () => {
+    const fred = bigInt(-0x12345AB)
+    const wilma = bigIntAsIntN(16, fred)
+    expect(bigInt.isInstance(wilma))
+    expect(wilma.toJSNumber()).toEqual(0xBA55)
+  })
+
+  test('correctly truncates positive to 16 bits', () => {
+    const fred = bigInt(0x12345AB)
+    const wilma = bigIntAsIntN(16, fred)
+    expect(bigInt.isInstance(wilma))
+    expect(wilma.toJSNumber()).toEqual(0x45AB)
+  })
+
+  test('correctly truncates negative to 8 bits', () => {
+    const fred = bigInt(-0x12345AB)
+    const wilma = bigIntAsIntN(8, fred)
+    expect(bigInt.isInstance(wilma))
+    expect(wilma.toJSNumber()).toEqual(0xD5)
+  })
+
+  test('correctly truncates positive to 8 bits', () => {
+    const fred = bigInt(0x12345AB)
+    const wilma = bigIntAsIntN(8, fred)
+    expect(bigInt.isInstance(wilma))
+    expect(wilma.toJSNumber()).toEqual(0xAB)
   })
 })
